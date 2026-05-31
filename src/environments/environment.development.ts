a4b7c8fb4be71ec1c722f2bf89858d85834cee29
asmaa-abobakr1 <asmaaahmed29699@gmail.com>
2026-05-31 14:52:26 +0300

    ةةةة

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://e-commerce-backend-91onih4qd-asmaa-abobakr1s-projects.vercel.app/api/v1'
+  apiUrl: 'https://e-commerce-backend-yourname.vercel.app/api/v1'
 };
