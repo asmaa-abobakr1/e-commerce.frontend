824f9a02c8d349aeb9da152f541085bd21745d58
asmaa-abobakr1 <asmaaahmed29699@gmail.com>
2026-05-31 16:06:49 +0300

    ةة

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://e-commerce-backend-five-lilac.vercel.app/api/v1'
+  apiUrl: '/api/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://e-commerce-backend-five-lilac.vercel.app/api'
+  apiUrl: '/api/v1'
 };
